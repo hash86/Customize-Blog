fa5803d1ffc19264e0f7df53301371c3e1be4499
Hamid Shoja <hamid.shoja@gmail.com>
2020-06-12 22:55:56 +0430

    initial commit

---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+import "./Blog.css";
+import Posts from "./Posts";
+
+class Blog extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/new-post">New Post</a>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <Posts />
+      </div>
+    );
+  }
+}
+
+export default Blog;
