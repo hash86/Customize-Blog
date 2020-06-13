8e9c0d2b063dd86db73ed8b0d18a8f3ffd6b2005
Hamid Shoja <hamid.shoja@gmail.com>
2020-06-13 23:47:34 +0430

    dynamic nested routes

--- src/containers/Blog/FullPost/FullPost.js
+++ src/containers/Blog/FullPost/FullPost.js
@@ -6,29 +6,38 @@ import "./FullPost.css";
 class FullPost extends Component {
   state = {
     loadedPost: null,
-    id: null,
   };
   componentDidMount() {
-    console.log("this.props", this.props);
+    this.loadData();
+  }
+
+  componentDidUpdate() {
+    this.loadData();
+  }
+
+  loadData() {
     if (this.props.match.params.id) {
-      console.log(this.props.match.params.id);
-      this.setState({ id: this.props.match.params.id });
-      axios.get("/posts/" + this.props.match.params.id).then((response) => {
-        // console.log(response);
-        this.setState({ loadedPost: response.data });
-      });
+      if (
+        !this.state.loadedPost ||
+        this.state.loadedPost.id !== +this.props.match.params.id
+      ) {
+        axios.get("/posts/" + this.props.match.params.id).then((response) => {
+          // console.log(response);
+          this.setState({ loadedPost: response.data });
+        });
+      }
     }
   }
 
   deletePostHandler = () => {
-    axios.delete("/posts/" + this.state.id).then((response) => {
+    axios.delete("/posts/" + this.props.match.params.id).then((response) => {
       console.log(response);
     });
   };
 
   render() {
     let post = <p style={{ textAlign: "center" }}>Please select a Post!</p>;
-    if (this.state.id) {
+    if (this.props.match.params.id) {
       post = <p style={{ textAlign: "center" }}>Loading...!</p>;
     }
     if (this.state.loadedPost) {
