b7573b7ac4310222f4510f6de039d6dcf8a17c2a
Hamid Shoja <hamid.shoja@gmail.com>
2020-06-13 22:28:06 +0430

    route params

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -4,6 +4,7 @@ import Posts from "./Posts";
 import { Route, Switch } from "react-router-dom";
 import Headers from "../../components/Headers";
 import NewPost from "./NewPost/NewPost";
+import FullPost from "./FullPost/FullPost";
 
 const Blog = (props) => {
   return (
@@ -12,6 +13,7 @@ const Blog = (props) => {
       <Switch>
         <Route path="/" exact component={Posts} />
         <Route path="/new-post" component={NewPost} />
+        <Route path="/:id" component={FullPost} />
       </Switch>
     </div>
   );
